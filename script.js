d4afe87fdbb8b3dc3f5451a9ccba7075066eabed
Akshay patra <akshaykumarpatra00@gmail.com>
2026-05-26 13:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 let line = document.getElementById("line");
 let deg = 0;
 setInterval(function (){
-line.style.transform = `rotate(${deg}deg)`;
+line.style.transform = `rotate(${deg}2deg)`;
 	deg += 2
 } , 20)
